dbe20ccd0bdb8efd3fd4951e99ad30987d749bd3
TheEternalShenron <rachit.gla_cs18@gla.ac.in>
2020-03-28 12:48:48 +0530

    updating insert function

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -44,4 +44,38 @@ class BST
         root = null;
 
     }
+
+    public void insert(int data)
+
+    {
+
+        root = insert(root, data);
+
+    }
+
+    private Node insert(Node node, int data)
+
+    {
+
+        if (node == null)
+
+            node = new Node(data);
+
+        else
+
+        {
+
+            if (data <= node.data)
+
+                node.left = insert(node.left, data);
+
+            else
+
+                node.right = insert(node.right, data);
+
+        }
+
+        return node;
+
+    }
 }
