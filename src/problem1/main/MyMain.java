2707e45cf8d37d18d5fe04a2ffa855c0d9b54f02
TheEternalShenron <rachit.gla_cs18@gla.ac.in>
2020-03-31 12:53:51 +0530

    updating in problem1

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -5,15 +5,22 @@
  *  Time: 7:17 PM
  */
 package problem1.main;
+
+import problem1.mybst.MyBinarySearchTree;
+
 // executable class
-
-import java.util.Scanner;
-
 public class MyMain {
-
-    public static void main(String[] args) {
-
-
+    public static void main(String[] args){
+        MyBinarySearchTree tree= new MyBinarySearchTree();
+        tree.insert(10);
+        tree.insert(96);
+        tree.insert(12);
+        System.out.println(23);
+        tree.insert(73);
+        tree.insert(14);
+        System.out.println("ALL Left Nodes are:");
+        tree.print_left_node();
+        System.out.println("Count of all Nodes which are not having left node is: ");
+        tree.count_not_left_node();
     }
-
 }
--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -6,210 +6,31 @@
  */
 package problem1.node;
 
-import java.util.Scanner;
-
-class Node
-
-{
-
-    Node left, right;
-
-    int data;
-
-
-    public Node(int n)
-
-    {
-
-        left = null;
-
-        right = null;
-
-        data = n;
-
+public class TreeNode {
+    private int data;
+    private TreeNode left;
+    private TreeNode right;
+    public void setdata(int data){
+        this.data = data;
     }
 
-}
-
-
-class BST
-
-{
-    private Node root;
-
-    public BST()
-
-    {
-
-        root = null;
-
+    public int getdata(){
+        return data;
     }
 
-    public void insert(int data)
-
-    {
-
-        root = insert(root, data);
-
+    public TreeNode getLeft(){
+        return left;
     }
 
-    private Node insert(Node node, int data)
-
-    {
-
-        if (node == null)
-
-            node = new Node(data);
-
-        else
-
-        {
-
-            if (data <= node.data)
-
-                node.left = insert(node.left, data);
-
-            else
-
-                node.right = insert(node.right, data);
-
-        }
-
-        return node;
-
+    public void setLeft(TreeNode left){
+        this.left = left;
     }
 
-    public void inorder()
-
-    {
-
-        inorder(root);
-
+    public TreeNode getRight(){
+        return right;
     }
 
-    private void inorder(Node r)
-
-    {
-
-        if (r != null)
-
-        {
-
-            inorder(r.left);
-
-            System.out.print(r.data +" ");
-
-            inorder(r.right);
-
-        }
-
+    public void setRight(TreeNode right){
+        this.right = right;
     }
-
-    public void preorder()
-
-    {
-
-        preorder(root);
-
-    }
-
-    private void preorder(Node r)
-
-    {
-
-        if (r != null)
-
-        {
-
-            System.out.print(r.data +" ");
-
-            preorder(r.left);
-
-            preorder(r.right);
-
-        }
-
-    }
-
-    public void postorder()
-
-    {
-
-        postorder(root);
-
-    }
-
-    private void postorder(Node r)
-
-    {
-
-        if (r != null)
-
-        {
-
-            postorder(r.left);
-
-            postorder(r.right);
-
-            System.out.print(r.data +" ");
-
-        }
-
-    }
-}
-
-class LinkedListBST
-
-{
-
-    public static void main(String[] args)
-
-    {
-
-        Scanner scan = new Scanner(System.in);
-
-        /* Creating object of BST */
-
-        BST bst = new BST();
-
-        System.out.println("Linked List Binary Search Tree Test\n");
-
-        char ch;
-
-        /*  Accept input  */
-
-        do
-
-        {
-
-            System.out.println("Enter integer element to insert");
-
-            bst.insert( scan.nextInt() );
-
-
-
-            /*  Display tree  */
-
-            System.out.print("\nPost order : ");
-
-            bst.postorder();
-
-            System.out.print("\nPre order : ");
-
-            bst.preorder();
-
-            System.out.print("\nIn order : ");
-
-            bst.inorder();
-
-
-
-            System.out.println("\nDo you want to continue (Type y or n) \n");
-
-            ch = scan.next().charAt(0);
-
-        } while (ch == 'Y'|| ch == 'y');
-
-    }
-
 }
