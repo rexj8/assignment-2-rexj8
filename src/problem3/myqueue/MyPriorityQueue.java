820f76b72b92ff9ab7412c441f79982f953edbb3
TheEternalShenron <rachit.gla_cs18@gla.ac.in>
2020-03-31 12:06:08 +0530

    updating Node class

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -18,10 +18,6 @@ class MyPriorityQueue
 
     public int size;
 
-
-
-    /* Constructor */
-
     public MyPriorityQueue()
 
     {
@@ -34,6 +30,120 @@ class MyPriorityQueue
 
     }
 
+    public boolean isEmpty()
 
+    {
+
+        return front == null;
+
+    }
+
+
+    public int getSize()
+
+    {
+
+        return size;
+
+    }
+
+
+    public void insert(int data)
+
+    {
+
+        Node nptr = new Node(data, null);
+
+        if (rear == null)
+
+        {
+
+            front = nptr;
+
+            rear = nptr;
+
+        }
+
+        else
+
+        {
+
+            rear.setLink(nptr);
+
+            rear = rear.getLink();
+
+        }
+
+        size++ ;
+
+    }
+
+    public int remove()
+
+    {
+
+        if (isEmpty() )
+
+            throw new NoSuchElementException("Underflow Exception");
+
+        Node ptr = front;
+
+        front = ptr.getLink();
+
+        if (front == null)
+
+            rear = null;
+
+        size-- ;
+
+        return ptr.getData();
+
+    }
+
+    public int peek()
+
+    {
+
+        if (isEmpty() )
+
+            throw new NoSuchElementException("Underflow Exception");
+
+        return front.getData();
+
+    }
+
+    public void display()
+
+    {
+
+        System.out.print("\nQueue = ");
+
+        if (size == 0)
+
+        {
+
+            System.out.print("Empty\n");
+
+            return ;
+
+        }
+
+        Node ptr = front;
+
+        while (ptr != rear.getLink() )
+
+        {
+
+            System.out.print(ptr.getData()+" ");
+
+            ptr = ptr.getLink();
+
+        }
+
+        System.out.println();
+
+    }
 
 }
+
+
